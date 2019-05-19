c1ed28531f95789940e452fc48245f8ee2238212
xin <504102723@qq.com>
2019-05-19 11:10:56 +0800

    generate by idea

---
+++ src/main/java/pers/xin/mpes/MpesApplication.java
@@ -0,0 +1,13 @@
+package pers.xin.mpes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpesApplication.class, args);
+    }
+
+}
